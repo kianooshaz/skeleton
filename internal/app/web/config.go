06f8693d066a2cbf1288e20a29f6fd29eff9f114
Kianoosh <kianoosh.az@gmail.com>
2024-09-12 18:09:32 +0330

    feature: add dynamic config

--- internal/app/web/config.go
+++ internal/app/web/config.go
@@ -1,6 +1,8 @@
 package web
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
 
 	"github.com/go-playground/validator/v10"
@@ -9,7 +11,7 @@ import (
 )
 
 type Config struct {
-	Version string `yaml:"version" env:"SKELETON_VERSION"`
+	Version string `yaml:"version" env:"SKELETON_VERSION" validate:"required"`
 }
 
 func newConfig(configPath string) (*Config, error) {
@@ -26,9 +28,39 @@ func newConfig(configPath string) (*Config, error) {
 		return nil, fmt.Errorf("error at feed config: %w", err)
 	}
 
-	if err := validator.New().Struct(&cfg); err != nil {
+	if err := cfg.Validate(); err != nil {
 		return nil, fmt.Errorf("error at validate config: %w", err)
 	}
 
+	backupCfg := bytes.Buffer{}
+	if err := gob.NewEncoder(&backupCfg).Encode(cfg); err != nil {
+		return nil, fmt.Errorf("cant encode config for backup: %w", err)
+	}
+
+	c.SetupListener(func(err error) {
+		// todo: lock application
+
+		if err := cfg.Validate(); err != nil {
+			if err := gob.NewDecoder(&backupCfg).Decode(&cfg); err != nil {
+				fmt.Printf("error at load backup config: %v", err)
+			}
+
+			fmt.Printf("reverted config, new config validation failed: %v\n", err.Error())
+			return
+		}
+
+		if err := gob.NewEncoder(&backupCfg).Encode(cfg); err != nil {
+			fmt.Printf("cant encode config for backup: %v\n", err.Error())
+		}
+	})
+
 	return &cfg, nil
+}
+
+func (c *Config) Validate() error {
+	return validator.New().Struct(c)
+}
+
+func (c *Config) Setup() error {
+	return nil
 }
