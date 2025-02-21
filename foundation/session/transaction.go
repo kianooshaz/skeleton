224c16b9065205a420641a3128a612e14ed2a46c
kianoosh <kianoosh.az@gmail.com>
2025-02-21 14:30:31 +0330

    add comment for session package

--- foundation/session/transaction.go
+++ foundation/session/transaction.go
@@ -8,6 +8,8 @@ import (
 
 type queryExecutorKey struct{}
 
+// GetQueryExecutor retrieves the query executor stored in the context.
+// If no query executor is found in the context, it returns the provided fallback value.
 func GetQueryExecutor(ctx context.Context, fallback database.QueryExecutorProtocol) database.QueryExecutorProtocol {
 	tx := ctx.Value(queryExecutorKey{})
 	if tx == nil {
@@ -16,6 +18,7 @@ func GetQueryExecutor(ctx context.Context, fallback database.QueryExecutorProtoc
 	return tx.(database.QueryExecutorProtocol)
 }
 
+// SetQueryExecutor stores the provided query executor in the context.
 func SetQueryExecutor(ctx context.Context, tx database.QueryExecutorProtocol) context.Context {
 	return context.WithValue(ctx, queryExecutorKey{}, tx)
 }
--- foundation/session/userid.go
+++ foundation/session/userid.go
@@ -6,11 +6,14 @@ import (
 
 type userIDKey struct{}
 
+// GetUserID retrieves the user ID stored in the context.
+// It returns the user ID and a boolean indicating whether the user ID was found.
 func GetUserID(ctx context.Context) (int, bool) {
 	userID, ok := ctx.Value(userIDKey{}).(int)
 	return userID, ok
 }
 
+// SetUserID stores the provided user ID in the context.
 func SetUserID(ctx context.Context, userID int) context.Context {
 	return context.WithValue(ctx, userIDKey{}, userID)
 }
