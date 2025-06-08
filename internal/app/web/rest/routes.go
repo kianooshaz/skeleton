101409355c17b446c391fcc17ce42d0206f2af4c
Kianoosh <kianoosh.az@gmail.com>
2025-06-08 16:45:12 +0330

    feat: add health check endpoint

--- internal/app/web/rest/routes.go
+++ internal/app/web/rest/routes.go
@@ -1,11 +1,7 @@
 package rest
 
-import (
-	"github.com/kianooshaz/skeleton/internal/app/web/rest/handler"
-)
-
 func (s *server) registerRoutes() {
-	s.core.GET("/health", handler.HealthCheck)
+	s.core.GET("/health", HealthCheck)
 
 	// s.core.GET("/user", registerHandler(userService.Service.Get))
 	// s.core.GET("/user/list", registerHandler(userService.Service.List))
